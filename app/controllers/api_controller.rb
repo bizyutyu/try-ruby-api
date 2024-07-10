2099779fc62e4ab5fb8f4f7f8231dbdc994d6738
bizyutyu <bizyuness.takakura@gmail.com>
2024-07-10 23:01:55 +0900

    add: エンドポイントの追加等

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,7 @@
 class ApiController < ApplicationController
     protect_from_forgery with: :null_session
-  
+
+    # 挨拶するエンドポイント
     def hello
       if params[:message] == "Hello"
         render json: { response: "Hello World!" }
@@ -8,4 +9,19 @@ class ApiController < ApplicationController
         render json: { response: "Invalid message" }, status: :bad_request
       end
     end
-  end
+
+    # RubyOnRails
+    def isRubyOnRails
+        if params[:message] == "RubyOnRails"
+          render plain: "RubyOnRails!!!"
+        else
+          render plain: "NotRubyOnRails..." , status: :bad_request
+        end
+    end
+
+    # 現在時刻を答えるエンドポイント
+    def currentTime
+        nowtime = Time.now
+        render plain: nowtime.to_s
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tweets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,4 +11,6 @@ Rails.application.routes.draw do
 
   # 新しいAPIエンドポイントのルート
   post "api/hello", to: "api#hello"
+  post "api/is-ruby-on-rails", to: "api#isRubyOnRails"
+  get "api/now-time", to: "api#currentTime"
 end
